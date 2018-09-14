fe8d2518856f30a96fb773ed148219eee9e194e5
hootan <hootan09@gmail.com>
2018-09-14 16:48:39 +0430

    working on upload encrypted

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const fs = require('fs');
+const zlib = require('zlib');
+const http = require('http');
+const crypto = require('crypto');
+const stream = require('stream');
+
+window.sendImage = function (file) {
+    const options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/upload/',
+        method: 'PUT',
+        headers: {
+            filename: 'pic.jpg',
+            'Content-Type': 'application/octet-stream',
+            'Content-Encoding': 'gzip'
+        }
+    };
+    const req = http.request(options, res => {
+        console.log('Server response: ' + res.statusCode);
+    });
+
+    var a = new stream.PassThrough()
+    a.write(file);
+    a.end();
+    a.pipe(zlib.createGzip())
+        .pipe(crypto.createCipher('aes192', 'niki'))
+        .pipe(req)
+        .on('finish', () => {
+            console.log('File successfully sent')
+        })
+        .on('error', (err) => {
+            console.log('error in send file ' + err);
+
+        });
+}
